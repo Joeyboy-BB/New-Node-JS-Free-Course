f6bbcd6738caa17364ab6e3aa2ec2e90482acc4a
Joeyboy-BB <boonnarin1@gmail.com>
2025-10-01 00:58:21 +0700

    Add all file off project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
+
+/*app.get("/Hello")*/
+
+/* จัดการ request ที่เข้ามาในหน้า / จะส่งข้อมูลอะไรกลับไป */
+app.get("/", (req, res) => {
+
+    /*res.send("Hello My World 555");*/
+    res.render("index", { username: "Joeyza55+", customers: ["A", "B", "C"] });
+
+})
+
+app.listen(PORT, () => {
+    debug("Listening on port", chalk.red(PORT));
+})
